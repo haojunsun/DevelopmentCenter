26f20db7262783407ca5a6be619237e805e63222
haojun.sun <coolboss1@sina.com>
2015-08-06 18:02:58 +0800

    添加文章模型

---
+++ DevelopmentCenter/DevelopmentCenter.Core/Repositories/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DevelopmentCenter.Core.Models;
+using DevelopmentCenter.Infrastructure;
+
+namespace DevelopmentCenter.Core.Repositories
+{
+    public class AppDbContext : DbContext, IDependencyPerRequest
+    {
+        public AppDbContext()
+            : base("DefaultConnection")
+        {
+        }
+
+        public DbSet<Article> Articles { get; set; }
+    }
+}
